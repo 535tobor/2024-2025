dd0d9e60df6757a83b4555dd973cfe713bc53d23
CHSRobotics <535tobor@gmail.com>
2024-11-14 17:50:11 -0500

    arm work in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamecode/teleop/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamecode/teleop/Main.java
@@ -20,6 +20,7 @@ public class Main extends OpMode {
     Servo claw;
     DcMotor shaft;
     DcMotor arm;
+    Boolean ONCELER = false;
 
     @Override
     public void init() {
@@ -40,17 +41,17 @@ public class Main extends OpMode {
     public void loop() {
         if (gamepad1.left_stick_y < 0) {  //this is flipped so the joystick will act normal for drivers//
         //forward//
-            fl.setPower(-1);
-            fr.setPower(-1);
-            bl.setPower(-1);
-            br.setPower(-1);
+            fl.setPower(-0.5);
+            fr.setPower(-0.5);
+            bl.setPower(-0.5);
+            br.setPower(-0.5);
 
         } else if (gamepad1.left_stick_y > 0) {
 
-            fl.setPower(1);
-            fr.setPower(1);
-            bl.setPower(1);
-            br.setPower(1);
+            fl.setPower(0.5);
+            fr.setPower(0.5);
+            bl.setPower(0.5);
+            br.setPower(0.5);
             //this will make the robot reverse //
 
         }
@@ -58,66 +59,73 @@ public class Main extends OpMode {
 
         if (gamepad1.right_stick_x > 0) {
             // when right joystick pulled right, robot turns right //
-            fl.setPower(1);
-            fr.setPower(-1);
-            bl.setPower(1);
-            br.setPower(-1);
+            fl.setPower(-0.5);
+            fr.setPower(0.5);
+            bl.setPower(-0.5);
+            br.setPower(0.5);
         }
 
         if (gamepad1.right_stick_x < 0) {
             // when right joystick pulled left, robot turns left //
-            fl.setPower(-1);
-            fr.setPower(1);
-            bl.setPower(-1);
-            br.setPower(1);
+            fl.setPower(0.5);
+            fr.setPower(-0.5);
+            bl.setPower(0.5);
+            br.setPower(-0.5);
 
 
         }
 
 
-          else if(gamepad1.dpad_up){
-            wrist.setPosition(6);
-
-        }
-        else if(gamepad1.dpad_down){
-
-            claw.setPosition(1);
-        }
-        //this is to close the claw//
-        else if(gamepad1.y){
-            claw.setPosition(0);
-
-        }
-
-        else if (gamepad1.right_stick_y < 0){
-
-            arm.setPower(1);
-
-        }
 
 
-        else if (gamepad1.right_stick_y > 0){
 
-            arm.setPower(-1);
-        }
-        else if (gamepad1.dpad_left){
+
+        else if (gamepad2.dpad_left){
 
             shaft.setPower(-1);
         }
 
-        else if (gamepad1.dpad_right){
+        else if (gamepad2.dpad_right){
 
             shaft.setPower(1);
         }
+
+
+         else if (gamepad2.a){
+
+             claw.setPosition(0);
+
+
+        }
+
+         else if (gamepad2.y){
+
+             claw.setPosition(1);
+        }
+
 
         else {
             fl.setPower(0);
             fr.setPower(0);
             bl.setPower(0);
             br.setPower(0);
-            arm.setPower(0);
             shaft.setPower(0);
         }
+
+
+
+         if (gamepad1.dpad_down){
+
+            arm.setPower(1);
+        }
+
+        else if (gamepad1.dpad_up){
+
+            arm.setPower(-1);
+        } else{
+             arm.setPower(0);
+
+         }
 
 
 
